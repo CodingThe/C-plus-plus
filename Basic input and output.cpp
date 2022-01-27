f1075f4fd4ef912c9233d8717c5692b9c04ca7d0
Prakash Thakur <89472789+CodingThe@users.noreply.github.com>
2022-01-27 13:43:35 +0530

    Add files via upload

---
+++ Basic input and output.cpp
@@ -0,0 +1,12 @@
+#include "iostream"
+using namespace std;
+int main(){
+    int a,b;
+cout<<"Enter the value which you want to add :"<<endl;
+cout<<" Enter the first value a :"<<endl;
+cin>>a;
+cout<<" Enter the second value b :"<<endl;
+cin>>b;
+cout<<"The sum of Entered values are : "<<a+b<<endl;
+return 0;
+}
