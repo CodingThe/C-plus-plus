15a756bea7e756f1b9f8f51c37cf2cbac1a9b36e
Prakash Thakur <89472789+CodingThe@users.noreply.github.com>
2022-01-27 13:44:40 +0530

    Add files via upload

---
+++ headers files & operators.cpp
@@ -0,0 +1,46 @@
+#include "iostream"
+using namespace std;
+int main(){
+    int a,b;
+cout<<"operators in C++ :"<<endl;
+cout<<endl;
+cout<<"Enter the value which you want to operat:"<<endl;
+cout<<" Enter the first value a :"<<endl;
+cin>>a;
+cout<<" Enter the second value b :"<<endl;
+cin>>b;
+
+// Uses of Arithmetic operators:-
+cout<<"The result value of given a+b is :"<<a+b<<endl;
+cout<<"The result value of given a-b is :"<<a-b<<endl;
+cout<<"The result value of given a*b is :"<<a*b<<endl;
+cout<<"The result value of given a/b is :"<<a/b<<endl;
+cout<<"The result value of given a%b is :"<<a%b<<endl;
+cout<<"The result value of given ++a is :"<<++a<<endl;
+cout<<"The result value of given --a is :"<<--a<<endl;
+cout<<"The result value of given a++ is :"<<a++<<endl;
+cout<<"The result value of given a-- is :"<<a--<<endl;
+cout<<endl;
+
+// Uses of Assignment operators:-
+//int a=;
+//char b=;
+
+
+//  Uses of Comparision operators:-
+cout<<"The result value of given  a== b is :"<<(a==b)<<endl;
+cout<<"The result value of given a != b is :"<<(a!=b)<<endl;
+cout<<"The result value of given a >= b is :"<<(a>=b)<<endl;
+cout<<"The result value of given a <= b is :"<<(a<=b)<<endl;
+cout<<"The result value of given a > b is :"<<(a>b)<<endl;
+cout<<"The result value of given a < b is :"<<(a<b)<<endl;
+cout<<endl;
+
+//Uses of Logical operators:-
+cout<<"The value of and logical operator ((a==b) && (a<b)) is:"<<((a==b) && (a<b))<<endl;
+cout<<"The value of and logical operator ((a==b) ||a<b))is:"<<((a==b) ||(a<b))<<endl;
+cout<<"The value of and logical operator (!a==b) is:"<<(!a==b)<<endl;
+return 0;
+
+}
+
