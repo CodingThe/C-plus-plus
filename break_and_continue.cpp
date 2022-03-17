ff0e4929fbde77978cda95b1c1b9999bab4c18ad
Prakash Thakur <89472789+CodingThe@users.noreply.github.com>
2022-03-17 22:11:00 +0530

    Add files via upload

---
+++ break_and_continue.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int
+ main(){
+	/*for(int i=0;i<50;i++){
+		if(i==5){
+			break;
+		}
+	
+       cout<<i<<" Hello World"<<endl;
+	
+		}*/
+		for(int i=0;i<50;i++){
+			if(i==5){
+				continue;
+			
+			}
+				cout<<i<<" Hello World"<<endl;
+		}
+		
+}
+
