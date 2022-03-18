816f5040924a95eb3f589e28e7879f997f0ac40d
Prakash Thakur <89472789+CodingThe@users.noreply.github.com>
2022-03-18 19:24:50 +0530

    Add files via upload

---
+++ loops_in_C++.cpp
@@ -0,0 +1,34 @@
+#include"iostream"
+using namespace std;
+int main(){
+	//For loop:-
+	
+	//for (int i=1;i<40;i++){
+	//	cout<<i<<endl;
+	 //}
+	 
+	 //infinite for loop:-
+	 
+     //for (int j=1;1<=2;j++){
+	 //	cout<<j<<" Hello World"<<endl;
+	 //}
+	 
+	 //While Loop:-
+	 
+	// int i=0;
+	// while(i<10){
+	// 	cout<<i<<endl;
+	// 	i++;
+	 //}
+	 
+	 //Do While loop:-
+	 int i=1;
+	 do
+	 {
+	 	cout<<i<<endl;
+	 	i++;
+	 }while(i<40);
+	 
+	 
+	return 0;
+}
