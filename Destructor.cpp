3f3e83e8f56dc045c9e425ebef13797d1a87cab7
Prakash Thakur <89472789+CodingThe@users.noreply.github.com>
2022-04-26 15:07:01 +0530

    Add files via upload

---
+++ Destructor.cpp
@@ -0,0 +1,29 @@
+#include"iostream"
+using namespace std;
+int count=0;
+class num{
+public:
+    num(){
+    count++;
+    cout<<"This is the time when constructor is called for object number"<<count<<endl;
+
+    }
+ ~num(){
+    count++;
+    cout<<"This is the time when Destructor is called for object number"<<count<<endl;
+count--;
+    }
+
+};
+int main(){
+cout<<"We are inside our main function"<<endl;
+cout<<"Creating first object n1"<<endl;
+num n1;{
+cout<<"Entering the object block"<<endl;
+cout<<"Creting two more objects"<<endl;
+ num n2,n3;
+cout<<"Exting this block"<<endl;
+}
+cout<<"Back to main"<<endl;
+return 0;
+}
