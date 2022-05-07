ad1687aaa8c1cf08e8955cb2051c145ed34e790d
Prakash Thakur <89472789+CodingThe@users.noreply.github.com>
2022-05-07 23:44:03 +0530

    Add files via upload

---
+++ New Keyword in pointers.cpp
@@ -0,0 +1,10 @@
+#include"iostream"
+using namespace std;
+int main(){
+float *a=new float(99.99);
+int *ptr= new int(98);
+cout<<"The value of a is :"<<*a<<endl;
+cout<<"The value of ptr is :"<<*ptr<<endl;
+return 0;
+}
+
