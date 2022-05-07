6ef7f9a3e1899e562139874c632b18fb8c126f58
Prakash Thakur <89472789+CodingThe@users.noreply.github.com>
2022-05-07 23:44:38 +0530

    Add files via upload

---
+++ new delete pointer.cpp
@@ -0,0 +1,16 @@
+#include"iostream"
+using namespace std;
+int main(){
+int *arr=new int[4];
+arr[0]=96;
+arr[1]=97;
+arr[2]=98;
+arr[3]=99;
+delete []arr;
+
+cout<<"The value of arr[0] is :"<<arr[0]<<endl;
+cout<<"The value of arr[1] is :"<<arr[1]<<endl;
+cout<<"The value of arr[2] is :"<<arr[2]<<endl;
+cout<<"The value of arr[3] is :"<<arr[3]<<endl;
+return 0;
+}
